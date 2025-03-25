255bf9ed4aad46296b15500bfba097edc6a69e1d
siyamol <siyamolmj.ptf@gmail.com>
2025-03-25 11:40:13 +0530

    atn

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  
-  transpileDependencies: true,
+  devServer:{
+    allowedHosts:"all"
+  },
 
   pluginOptions: {
     vuetify: {
