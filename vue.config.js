6c62eb5c2eff1e9c3bb8c8d5c0b930920b92be5a
siyamol <siyamolmj.ptf@gmail.com>
2025-04-10 16:09:03 +0530

    fix:env

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer:{
-    allowedHosts:"all"
+  transpileDependencies: true,
+ devServer: {
+        allowedHosts:"all",
+        port: process.env.VUE_APP_PORT , 
   },
-
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
